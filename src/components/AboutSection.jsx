e159b51900c0419c1081595ddd232493090f35db
JoLe2004 <s.johnle2004@gmail.com>
2025-12-09 21:30:36 -0500

    Update job title and field in AboutSection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -10,14 +10,14 @@ export default function AboutSection() {
 
                 <div className="grid grid-cols-1 gap-12 items-center">
                     <div className="space-y-6">
-                        <h3 className="text-2xl font-semibold">Software Developer</h3>
+                        <h3 className="text-2xl font-semibold">Software/Web Developer</h3>
                         <p>
                             I am currently a student at Grand Valley State University majoring in Computer Science with a minor in Math, and I'm expected to graduate in Fall of 2026.
                             Some of the classes I have taken include Web Application Programming, Mobile Application Development, and Secure Software Engineering, giving me the tools
                             and skills I need to succeed as software developer. My hobbies include cooking, playing videogames, and traveling. 
                         </p>
                         <p>
-                            My short-term goals include graduating and getting an internship/job in the software development field. In the long-term, however, I hope to continue learning
+                            My short-term goals include graduating and getting an internship/job in the software or web development field. In the long-term, however, I hope to continue learning
                             new skills that will help me in my career. I also hope to dabble in videogame development and maybe make my own game one day.
                         </p>
 
